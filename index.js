52edcd3b21206d4a2e353f996972368e711df0b6
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-04-24 23:12:01 -0700

    getting methods ready for api rendering

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-let postsArray = []
 
 
 
 
+// renderPosts(){
+//     let html = ''
+
+//     for(let post of postsArray){
+//         ``
+//     }
+// }
 
 fetch("https://apis.scrimba.com/placeholder/posts")
     .then(requests => requests.json())
-    .then(data => {
-        console.log(data)
+    .then(post => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for(post of postsArr){
+            html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
     })
 
 
 
+let options = {
+    title: postTitle,
+    body: postBody
+}
+
+
+fetch("https://apis.scrimba.com/placeholder/posts")
 
